60bdc908fe449a605f0fc76aff3df0cb3439d5a9
John Biesnecker <jbiesnecker@gmail.com>
2021-12-26 15:41:35 -0800

    Initial commit

---
+++ iqtest.c
@@ -0,0 +1,119 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static const int ROW_LENGTHS[] = {1, 2, 3, 4, 5};
+static const int ROW_STARTS[] = {0, 1, 3, 6, 10};
+
+static const int N_ROWS = 5;
+
+static const uint16_t FULL_BOARD = 0x7fff;
+
+typedef struct jump {
+    uint16_t start;
+    uint16_t middle;
+    uint16_t end;
+} jump;
+
+typedef struct state {
+    int emptyRow;
+    int emptyHole;
+    uint16_t board;
+    uint8_t moveIdx;
+    uint8_t moves[13];
+} state;
+
+static uint16_t holeMask(int row, int hole) {
+    return (1U << (ROW_STARTS[row] + hole));
+}
+
+static bool isValidRowHole(int row, int hole) {
+    return row >= 0 && row < N_ROWS && hole >= 0 && hole < ROW_LENGTHS[row];
+}
+
+static int enumerateJumps(jump *jumps) {
+    int jidx = 0;
+    for (int row = 0; row < N_ROWS; ++row) {
+        for (int hole = 0; hole < ROW_LENGTHS[row]; ++hole) {
+            const uint16_t start_mask = holeMask(row, hole);
+
+            static const int middle_rows[] = {-1, -1, 0, 0, 1, 1};
+            static const int middle_holes[] = {-1, 1, -1, 1, 0, 1};
+            static const int end_rows[] = {-2, -2, 0, 0, 2, 2};
+            static const int end_holes[] = {-2, 2, -2, 2, 0, 2};
+
+            for (int i = 0; i < 6; ++i) {
+                const int middle_row = row + middle_rows[i];
+                const int middle_hole = hole + middle_holes[i];
+                const int end_row = row + end_rows[i];
+                const int end_hole = hole + end_holes[i];
+
+                if (isValidRowHole(middle_row, middle_hole) &&
+                    isValidRowHole(end_row, end_hole)) {
+                    jumps[jidx].start = start_mask;
+                    jumps[jidx].middle = holeMask(middle_row, middle_hole);
+                    jumps[jidx].end = holeMask(end_row, end_hole);
+                    ++jidx;
+                }
+            }
+        }
+    }
+    return jidx;
+}
+
+void step(int *outcomes, int noutcomes, jump *jumps, int njumps, state *s) {
+    const int pop = __builtin_popcount(s->board);
+    if (pop == 1) {
+        outcomes[1]++;
+        free(s);
+        return;
+    }
+    bool didJump = false;
+    for (int jidx = 0; jidx < njumps; ++jidx) {
+        if ((s->board & jumps[jidx].start) && (s->board & jumps[jidx].middle) &&
+            (~(s->board) & jumps[jidx].end)) {
+            // This is a valid jump.
+            didJump = true;
+            state *newstate = calloc(1, sizeof(state));
+            memcpy(newstate, s, sizeof(state));
+
+            newstate->moves[newstate->moveIdx++] = (uint8_t)jidx;
+            newstate->board =
+                (((s->board & ~jumps[jidx].start) & ~jumps[jidx].middle) |
+                 jumps[jidx].end);
+            step(outcomes, noutcomes, jumps, njumps, newstate);
+        }
+    }
+    // Didn't jump, so this is an end state.
+    if (!didJump) {
+        outcomes[pop]++;
+    }
+
+    free(s);
+}
+
+int main(int argc, char **argv) {
+    int outcomes[15] = {0};
+    jump jumps[31];
+    const int njumps = enumerateJumps(jumps);
+    for (int row = 0; row < N_ROWS; ++row) {
+        for (int hole = 0; hole < ROW_LENGTHS[row]; ++hole) {
+            state *s = calloc(1, sizeof(state));
+            s->emptyRow = row;
+            s->emptyHole = hole;
+            s->board = FULL_BOARD & ~holeMask(row, hole);
+
+            step(outcomes, 15, jumps, njumps, s);
+        }
+    }
+
+    int total = 0;
+    for (int i = 0; i < 15; i++) {
+        printf("%d left: %d\n", i, outcomes[i]);
+        total += outcomes[i];
+    }
+    printf("Total outcomes: %d\n", total);
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Original IQ Test Solver
+
+While at a restaurant in Pismo Beach, California, my kids found a little peg
+board game called "The Original IQ Tester." There is a triangle with 15 holes,
+one of which is empty. Each move you can jump one peg over another into an
+empty hole, like in checkers. The goal is to end up with one peg left.
+
+It made me curious how many outcomes lead to a single peg remaining, and how
+many different games there were, so I wrote some code.
+
+Total games: 1167320
+
+Outcomes:
+
+- 1 left: 17702
+- 2 left: 192679
+- 3 left: 471383
+- 4 left: 384524
+- 5 left: 87984
+- 6 left: 10956
+- 7 left: 1851
+- 8 left: 195
+- 9 left: 38
+- 10 left: 7
+- 11 left: 1
+- 12 left: 0
+- 13 left: 0
+- 14 left: 0
